38b437e80390b4803413eaaee516c2295dad5638
Trinath Reddy <trinathreddy158@gmail.com>
2023-11-14 13:19:07 +0530

    Unique Triplets.py
    # sort, nest loop, two pointer approach - O(n^2) ,O(1)

--- Arrays/Unique Triplets.py
+++ Arrays/Unique Triplets.py
@@ -6,6 +6,45 @@ Input: nums = [0, -1, 2, -3, 1]
 Output: [0, -1, 1], [2, -3, 1]
 
 """
+# sort, nest loop, two pointer approach - O(n^2) ,O(1)
+def threeUniqueTriplets(nums, target):
+    nums.sort()
+    result = []
+
+    for i in range(len(nums) -2):
+
+        if i >0 and nums[i] == nums[i-1]:
+            continue # skip duplicates
+
+        left , right = i+1, len(nums)-1
+
+        while left < right:
+
+            current_sum = nums[i] + nums[left] + nums[right]
+
+            if current_sum == target:
+                result.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left+1]:
+                    left += 1 # skip duplicates
+                while left < right and nums[right] == nums[right-1]:
+                    right -= 1 # skip duplicates
+
+                left +=1
+                right -= 1
+
+            elif current_sum < target:
+                left +=1
+            else:
+                right -=1
+
+        return result
+
+nums = [1,0,-1, 0,-2, 2,3,4]
+target = 2
+print(threeUniqueTriplets(nums, target))
+
+# solution -2 
 def sum_of_tripltes():
 
 def Unique_Triplets():
