6d11218ec65f937d44cb4f830078b4b4aa44031f
Trinath Reddy <trinathreddy158@gmail.com>
2023-11-14 11:52:58 +0530

    two_sum.py - different solutions

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -21,3 +21,24 @@ arr = [1,3,5,6,4]
 target = 7
 
 print(t.find_indexes(arr, target))
+
+
+# Solution -2: if the array is sorted , time complexity is O(n) even though sorted array is O(nlogn), space complexity is O(1)
+
+def two_sum_sorted(nums, target):
+
+    left, right = 0, len(nums)-1
+
+    while left < right:
+
+        current_sum = nums[left]+nums[right]
+        if current_sum == target:
+            return [left, right]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+
+    return None
+
+    
